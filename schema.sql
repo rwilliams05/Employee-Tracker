09fd139701aeabaee18ce8c5e6eb69afa559d460
rwilliams05 <71559818+rwilliams05@users.noreply.github.com>
2022-06-14 08:08:09 -0400

    add auto increment

--- schema.sql
+++ schema.sql
@@ -4,12 +4,12 @@ CREATE DATABASE employees_db;
 USE employees_db;
 
 CREATE TABLE department (
-    id INT  PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30)
 );
 
 CREATE TABLE role (
-    id INT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
     title VARCHAR(30),
     salary DECIMAL,
     department_id INT,
@@ -19,7 +19,7 @@ CREATE TABLE role (
 );
 
 CREATE TABLE employee (
-    id INT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
--- seeds.sql
+++ seeds.sql
@@ -1,27 +1,27 @@
-INSERT INTO department (id, name)
-VALUES (1, "Finance"),
-       (2, "Engineering"),
-       (3, "Legal"),
-       (4, "Sales");
+INSERT INTO department (name)
+VALUES ("Finance"),
+       ("Engineering"),
+       ("Legal"),
+       ("Sales");
 
 
-INSERT INTO role (id, title, salary, department_id)
-VALUES (1, "Account Manager", 120000, 1),
-       (2, "Accountant", 80000, 1),
-       (3, "Lead Engineer", 120000, 2),
-       (4, "Software Engineer", 100000, 2),
-       (5, "Legal Team Lead", 120000, 3),
-       (6, "Lawyer", 100000, 3),
-       (7, "Sales Lead", 120000, 4),
-       (8, "Sales Person", 80000, 4);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Account Manager", 120000, 1),
+       ("Accountant", 80000, 1),
+       ("Lead Engineer", 120000, 2),
+       ("Software Engineer", 100000, 2),
+       ("Legal Team Lead", 120000, 3),
+       ("Lawyer", 100000, 3),
+       ("Sales Lead", 120000, 4),
+       ( "Sales Person", 80000, 4);
 
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES (1, "Adam", "Smith", 1, NULL),
-       (2, "Loretta", "Castorini", 2, 1),
-       (3, "Lisbeth", "Salander", 3, NULL),
-       (4, "Montgomery", "Scott", 4, 3),
-       (5, "Atticus", "Finch", 5, NULL),
-       (6, "Saul", "Goodman", 6, 5),
-       (7, "Gordon", "Gekko", 7, NULL),
-       (8,"Willy", "Loman",8, 7);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Adam", "Smith", 1, NULL),
+       ("Loretta", "Castorini", 2, 1),
+       ("Lisbeth", "Salander", 3, NULL),
+       ("Montgomery", "Scott", 4, 3),
+       ("Atticus", "Finch", 5, NULL),
+       ("Saul", "Goodman", 6, 5),
+       ("Gordon", "Gekko", 7, NULL),
+       ("Willy", "Loman",8, 7);
       
